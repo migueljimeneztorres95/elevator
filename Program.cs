e386e3a801acd238a3800b50d2f4c296fec28305
MiguelJack <migueljimeneztorres95@gmail.com>
2023-03-07 13:16:51 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Solution {
+	static int fib(int n) {
+		if (n <= 1) {
+      return n;
+    }
+		return fib(n - 1) + fib(n - 2);
+	}
+
+	static public void Main() {
+		Console.Write("Number of floors: ");
+    string value = Console.ReadLine();
+    int A = Convert.ToInt32(value);
+		Console.WriteLine("Number of distinct ways: " + fib(A + 1));
+	}
+}
